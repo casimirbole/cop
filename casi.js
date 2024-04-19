9d55a6fe6c4d303ec57fd6ff63f573ad04763f44
CASIMIR <142940409+casimirbole@users.noreply.github.com>
2024-04-19 21:38:19 +0200

    Add files via upload

---
+++ casi.js
@@ -0,0 +1,164 @@
+
+
+
+function lame() {
+    var gloire =document.getElementById('gloire');
+
+    if (gloire.style.display==="none") {
+        gloire.style.display="block";  
+    }  
+    var bloc =document.getElementById("nc");
+        bloc.style.display ="none"
+
+        var bloc=document.getElementById("llc");
+        llc.style.display="none"; 
+
+        
+        var bloc =document.getElementById("monloc");
+
+        if (bloc.style.display==="block") {
+            bloc.style.display ="none"; 
+    
+        }
+        var finish =document.getElementById('finish');
+
+        if (finish.style.display==="block") {
+            finish.style.display="none";  
+        } 
+}
+
+
+
+function lae() {
+    var llc =document.getElementById('llc');
+
+    if (llc.style.display==="none") {
+        llc.style.display="block";  
+    }  
+    var bloc =document.getElementById("nc");
+    bloc.style.display ="none";
+
+    var gloire =document.getElementById('gloire');
+
+    if (gloire.style.display==="block") {
+        gloire.style.display="none";  
+
+    }
+    var bloc =document.getElementById("monloc");
+
+    if (bloc.style.display==="block") {
+        bloc.style.display ="none"; 
+
+    }
+    var finish =document.getElementById('finish');
+
+    if (finish.style.display==="block") {
+        finish.style.display="none";  
+    } 
+}
+
+
+
+
+function toggle() {
+    var bloc =document.getElementById("monloc");
+
+    if (bloc.style.display==="none") {
+        bloc.style.display ="block"; 
+
+    }
+        var bb =document.getElementById('nc');
+          if (bb.style.display==="block") {
+        bb.style.display ="none"; 
+
+    }
+
+    var gloire =document.getElementById('gloire');
+
+    if (gloire.style.display==="block") {
+        gloire.style.display="none";  
+    } 
+
+    var llc =document.getElementById('llc');
+
+    if (llc.style.display==="block") {
+        llc.style.display="none";  
+    }  
+    var finish =document.getElementById('finish');
+
+    if (finish.style.display==="block") {
+        finish.style.display="none";  
+    } 
+
+        
+
+
+}
+
+function retour() {
+    var bb =document.getElementById('nc');
+    if (bb.style.display==="none") 
+  bb.style.display ="block"; 
+
+
+  var bloc =document.getElementById("monloc");
+
+  if (bloc.style.display==="block") {
+      bloc.style.display ="none"; 
+
+  }
+
+
+  var llc =document.getElementById('llc');
+
+  if (llc.style.display==="block") {
+      llc.style.display="none";  
+  }  
+
+ var gloire =document.getElementById('gloire');
+
+    if (gloire.style.display==="block") {
+        gloire.style.display="none";  
+    } 
+
+    var finish =document.getElementById('finish');
+
+    if (finish.style.display==="block") {
+        finish.style.display="none";  
+    } 
+
+
+
+}
+
+
+function pm() {
+    var finish =document.getElementById('finish');
+
+    if (finish.style.display==="none") {
+        finish.style.display="block";  
+    } 
+
+    var bloc =document.getElementById("monloc");
+
+    if (bloc.style.display==="block") {
+        bloc.style.display ="none"; 
+
+    }
+
+    var gloire =document.getElementById('gloire');
+
+    if (gloire.style.display==="block") {
+        gloire.style.display="none";  
+    }
+    var llc =document.getElementById('llc');
+
+    if (llc.style.display==="block") {
+        llc.style.display="none";  
+    } 
+
+    var bb =document.getElementById('nc');
+    if (bb.style.display==="block") 
+      bb.style.display ="none"; 
+
+}
